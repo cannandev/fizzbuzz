a2c5f84e501fc76959338748290dfa518c57d9e3
Claire Annan <claire.annan.me@gmail.com>
2015-09-28 18:59:09 -0400

    Adding fizzbuzz game files.

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	for (var num = 1; num < 101; num++) {
+		if ((num % 3) === 0 && (num % 5) === 0) {
+			$('body').append('<p>FizzBuzz</p>');
+		}
+		else if ((num % 3) === 0) {
+			$('body').append('<p>Fizz</p>');
+		}
+		else if ((num % 5) === 0) {
+			$('body').append('<p>Buzz</p>');
+		}
+		else {
+			$('body').append('<p>' + num + '</p>');
+		}		
+	}
+});
