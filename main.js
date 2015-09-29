512bee799ca555d6ef1b365efd903c4ac1566430
Claire Annan <claire.annan.me@gmail.com>
2015-09-29 00:21:18 -0400

    Changing the for loop to condition to equal 100.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	for (var num = 1; num < 101; num++) {
+	for (var num = 1; num <= 100; num++) {
 		if ((num % 3) === 0 && (num % 5) === 0) {
 			$('body').append('<p>FizzBuzz</p>');
 		}
